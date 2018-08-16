12e910458f6dc16ce12f83342c7868c7b5a9555d
Michele Giorgi <io@michelegiorgi.com>
2018-08-16 22:11:20 +0200

    .

--- assets/scripts/core/ui.js
+++ assets/scripts/core/ui.js
@@ -3,7 +3,7 @@ import el from '../utils/elements'
 export default {
   focus() {
 		$(el("field")).click(function() { focusToggle(this); })
-		$(el("field", true, " input") + ", " + el("field", true, " textarea")).on("focus", function() { focusToggle(this, true); })
+		$(el("field", true, " :input")).on("focus", function() { focusToggle(this, true); })
 		function focusToggle(field, input = false) {
 			$(el("field_focus")).removeClass(el("field_focus", false))
 			if(input){
@@ -13,18 +13,30 @@ export default {
 				$(field).find("input, textarea").focus()
 			}
 		}
+		//autofocus first input
 		setTimeout(function(){
 			$(el("field") + ":first-child").click();
 		}, 1000);
+		$(el("field", true, " :input")).on("keyup", function(e) {
+			if($(this).val()) {
+				$(this).addClass("filled");
+			} else {
+				if($(this).hasClass("filled")) {
+					$(this).removeClass("filled");
+				} else if(e.keyCode == 8) {
+					$(this).prev("input").focus();
+				}
+			}
+		});
   },
   placeholder() {
-		$(el("input") + " *[placeholder]").each(function(){
+		$(el("input") + " :input[placeholder]").each(function(){
 			let placeholder = $(this).attr("placeholder")
 			$(this).closest(el("input")).attr("data-placeholder", placeholder)
 		})
 	},
 	filled() {
-		$(el("field", true, " input") + ", " + el("field", true, " textarea")).on("change", function() { fillToggle(this); })
+		$(el("field", true, " :input")).on("change", function() { fillToggle(this); })
 		function fillToggle(field) {
 			let val = $(field).val();
 			if(val) {
