a2b606d3c229196f911f41498d1e029ff7025fa7
Michele Giorgi <io@michelegiorgi.com>
2019-12-17 21:46:23 +0100

    various fixes

--- assets/scripts/core/embed.js
+++ assets/scripts/core/embed.js
@@ -6,17 +6,27 @@ export default {
       const formlink = $(this).attr('href')
       const formid = $(this).attr('id')
       if(!$('.formality__sidebar[data-sidebar='+formid+']').length) {
-        $('body').append('<div class="formality__sidebar" data-sidebar="'+formid+'"><div class="formality__sidebar__iframe"><iframe src="'+formlink+'"></iframe></div></div>')
+        $('body').append('<div class="formality__sidebar '+formid+'" data-sidebar="'+formid+'"><div class="formality__sidebar__iframe"><iframe src="'+formlink+'"></iframe></div></div>')
       }
     })
-    $('.formality__sidebar').addClass('formality__sidebar--loaded')
+    setTimeout(function(){ $('.formality__sidebar').addClass('formality__sidebar--loaded') }, 2000)
     this.openSidebar()
+    this.closeSidebar()
   },
   openSidebar() {
     $('.formality__cta').click(function(e){
       e.preventDefault()
       const formid = $(this).attr('id')
-      $('.formality__sidebar[data-sidebar='+formid+']').toggleClass('formality__sidebar--open')
+      $('.formality__sidebar[data-sidebar='+formid+']').addClass('formality__sidebar--open')
+      let iframe = $('.formality__sidebar[data-sidebar=' + formid + '] iframe')[0];
+      iframe.contentWindow.focus();
+    })
+  },
+  closeSidebar() {
+    $('.formality__sidebar').click(function(e){
+      e.stopPropagation();
+      e.preventDefault();
+      $(this).removeClass('formality__sidebar--open')
     })
   },
 }
