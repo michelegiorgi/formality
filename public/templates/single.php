28e16339b8f3e3ee46b5d620163944a6d86495a8
Michele Giorgi <io@michelegiorgi.com>
2021-01-09 09:00:03 +0100

    single template fixes

--- public/templates/single.php
+++ public/templates/single.php
@@ -7,10 +7,11 @@
 <html class="html-formality" <?php language_attributes(); ?>>
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
     <?php wp_head(); ?>
   </head>
-  <body <?php body_class(); ?>> 
+  <body <?php body_class(); ?>>
     <?php while ( have_posts() ) : the_post(); the_content(); endwhile; ?>
     <?php wp_footer(); ?>
   </body>
